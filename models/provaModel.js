65417c99d090553b32e4852217643ae4ba3e8302
DeniseLopes <denise@kosloski.com.br>
2019-09-02 22:17:54 -0300

    segunda parte

--- models/provaModel.js
+++ models/provaModel.js
@@ -1,5 +1,11 @@
-const provas = []
+const path = require('path')
+const fs = require('fs')
 
+const dbPath = path.join(
+    path.dirname(process.mainModule.filename),
+    'data',
+    'provas.json'
+)
 class Prova{
     constructor(disciplina, professor, dataProva){
         this.disciplina = disciplina
@@ -7,10 +13,22 @@ class Prova{
         this.dataProva = dataProva
     }
     salvar(){
+        let provas = []
+        try{
+        let data = fs.readFileSync(dbPath)
+            provas = JSON.parse(data.toString())
+        }catch(e){}
+        
         provas.push(this)
+        fs.writeFileSync(dbPath, JSON.stringify(provas))
     }
 
     static listar(){
+        let provas
+        try{
+            let data = fs.readFileSync(dbPath)
+                provas = JSON.parse(data.toString())
+            }catch(e){}
         return provas
     }
 }
